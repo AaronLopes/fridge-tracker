4e4707dae76f53de345ff09f751e0ece7ea2d9d4
AaronLopes <aaronlopes@me.com>
2019-10-13 18:23:19 -0400

    upload struct finished

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import gspread
+import itertools
 import numpy as np
 import pprint
 
@@ -17,10 +18,11 @@ client = gspread.authorize(creds)
 sheet = client.open('Fridge Data')
 
 """
-GOOGLE SHEETS API INFO
+GSPREAD API REF
 
-spreadsheet data are type string 
-sheet.insert_row(row, index)
+spreadsheet data type string 
+sheet.insert_row(values, index=1, value_input_option='RAW'),
+pushes current content down 
 sheet.delete()
 
 """
@@ -45,47 +47,67 @@ def get_date_T(date):
                 values[i] += [data[j][2]]
             working_channels += [i]
         except Exception as e:
+            print('Error: ' + str(e))
             print('No data for channel ' + str(i + 1) + ' on ' + date)
-            print('Error:' + str(e))
-
     return datetimes, values, working_channels
 
-# get current date
-# datetime.now().strftime('%y-%m-%d'), current date for testing
 
-date = '17-02-28'
-datetimes = [[], [], [], [], [], []]
-values = [[], [], [], [], [], []]
-datetimes_temp, values_temp, working_channels = get_date_T(date)
+def merge_datetimes_temp(datetimes, values, working_ch):
+    date_val_ch = [[], [], [], [], [], []]
+    missing_ch = []
+    flat_datetimes = list(set(itertools.chain(*datetimes)))
 
-if len(working_channels) is not 0:
+    # num channels
     for i in range(6):
-        datetimes[i] += datetimes_temp[i]
-        values[i] += values_temp[i]
+        if i in working_ch:
+            print('T CH ' + str(i + 1))
+            date_val = np.array([datetimes[i], values[i]])
+            date_val = date_val.transpose()
+            #pprint.pprint(date_val)
+            date_val_ch[i] = date_val
+        else:
+            print('No data found for CH ' + str(i + 1))
+            missing_ch.append(i)
 
-def merge_datetimes_temp(datetimes, values):
-    pass
-
-
-# fill in 0s in data
-for i in range(6):
-    print('T CH ' + str(i + 1))
-    for j in range(1, len(values[i])):
-        if float(values[i][j]) == 0.0 and float(values[i][j - 1]) != 0.0:
-            values[i][j] = values[i][j - 1]
-    """date_val = np.array([datetimes[i], values[i]])
-    date_val = date_val.transpose()
-    pprint.pprint('shape before transpose' + str(date_val.shape))
-    pprint.pprint(date_val)"""
-    print(datetimes[i])
-    print(values[i])
-
-if __name__ == 'main':
+    print(len(date_val_ch), len(date_val_ch[0]), len(date_val_ch[0][0]))
+    print(date_val_ch[0][0])
 
     """
-    schedule inside while True: 
-        every 10 minutes,
-        get current datetime, parse log files at that date
-        update on sheets
+    upload_struct = {
+        datetime.datetime(0,0,0,0,0,0) = [CH1 T, CH2 T, ..., CH6 T]
+        .
+        .
+        datetime.datetime(0,0,0,0,0,0) = [CH1 T, CH2 T, ..., CH6 T]
+    } 
     """
-    pass
+    upload_struct = dict((i, []) for i in flat_datetimes)
+    # pprint.pprint(upload_struct)
+
+    for i in range(len(date_val_ch)):
+        if len(date_val_ch[i]) == 0:
+            print('skipping CH ' + str(i + 1) + ', no available data')
+        else:
+            print('accessing CH ' + str(i + 1))
+            for j in range(len(date_val_ch[i])):
+                upload_struct[date_val_ch[i][j][0]] += [date_val_ch[i][j][1]]
+    pprint.pprint(upload_struct)
+
+
+if __name__ == '__main__':
+    # get current date
+    # datetime.now().strftime('%y-%m-%d'), date for testing
+    date = '17-02-28'
+    datetimes = [[], [], [], [], [], []]
+    values = [[], [], [], [], [], []]
+    datetimes_temp, values_temp, working_channels = get_date_T(date)
+    if len(working_channels) is not 0:
+        for i in range(6):
+            datetimes[i] += datetimes_temp[i]
+            values[i] += values_temp[i]
+    # fill in 0s in data
+    for i in range(6):
+        for j in range(1, len(values[i])):
+            if float(values[i][j]) == 0.0 and float(values[i][j - 1]) != 0.0:
+                values[i][j] = values[i][j - 1]
+
+    merge_datetimes_temp(datetimes, values, working_channels)
