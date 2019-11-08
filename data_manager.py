97ed8853b2a05c276f639a495634da9f1181f8ee
AaronLopes <aaronlopes@me.com>
2019-11-08 16:35:52 -0500

    added parsing for maxiguage files

--- data_manager.py
+++ data_manager.py
@@ -16,16 +16,14 @@ scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
 
 creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-client = gspread.authorize(creds)
 
 
-def parse_file(date, type, interval):
+def parse_file(date, data_type, num_channels):
     """
     Parses values over given interval. Creates datetime array, values array, and identifies
     channels missing data.
     :param date: date in format 'yy-mm-dd' corresponding to log folders
-    :param type: the file type of values to be parsed (T, K, P, etc.), should be in format ' X '
-    :param interval:
+    :param data_type: the file type of values to be parsed (T, K, P, etc.), should be in format ' X '
     :return: datetime objects corresponding to values parsed, values, and missing channels
     :rtype: lists
     """
@@ -35,10 +33,10 @@ def parse_file(date, type, interval):
     global num_uploads
 
     print('gathering data for upload number ' + str(num_uploads))
-    for i in range(6):
+    for i in range(num_channels):
         try:
             data = np.loadtxt('./logs/' + date + '/CH' + str(i + 1)
-                              + type + date + '.log', dtype=str, delimiter=',')
+                              + data_type + date + '.log', dtype=str, delimiter=',')
             n = len(data)
             for j in range(n):
                 year = int(data[j][0][7:9]) + 2000
@@ -48,7 +46,7 @@ def parse_file(date, type, interval):
                 minute = int(data[j][1][3:5])
                 second = int(data[j][1][6:8])
                 dt = datetime(year, month, day, hour, minute, second)
-                interval = datetime.now() - timedelta(minutes=interval)
+                interval = datetime.now() - timedelta(minutes=10)
                 if dt >= interval:
                     datetimes[i] += [dt]
                     values[i] += [data[j][2]]
@@ -59,14 +57,52 @@ def parse_file(date, type, interval):
     num_uploads += 1
     return datetimes, values, missing_channels
 
+def parse_maxigauge(date):
+    """
+    Parses maxigauge files and returns channels 
+    :param date: date in format 'yy-mm-dd' corresponding to log folders
+    :param data_type: the file type of values to be parsed (T, K, P, etc.), should be in format ' X '
+    :return: datetime objects corresponding to values parsed, values, and missing channels
+    :rtype: List
+    """
+    datetimes = [[], [], [], [], [], []]
+    values = [[], [], [], [], [], []]
+    missing_channels = []
+    try:
+        data = np.loadtxt('./logs/' + date + '/maxigauge ' + date +'.log', dtype=str, delimiter=',')
+        for i in range(len(data)):
+            time = data[i][1]
+            for j in range(6):
+                try:
+                    year = int(date[0:2]) + 2000
+                    month = int(date[3:5])
+                    day = int(date[6:9]) 
+                    hour = int(time[0:2])
+                    minute = int(time[3:5])
+                    second = int(time[6:9])
+                    dt = datetime(year, month, day, hour, minute, second)
+                    interval = datetime(17, 2, 28, 23, 49, 22)
+                    if dt > interval:
+                        values[j] += [data[i][j + 5*(j + 1)]]
+                        datetimes[j] += [dt]
+                except Exception as e:
+                    print('Exception: ' + str(e))
+                    print('data missing on CH ' + j + 'at time' + time)
+                    if j not in missing_channels:
+                        missing_channels += [j]
+    except Exception as e:
+        print('Error: ' + str(e))
+        print('Maxigauge file missing for date: ' + date)
+    return datetimes, values, missing_channels
 
-def restruct_data(datetimes, values, missing_ch):
+
+def restructure_data(datetimes, values, missing_ch):
     date_val_ch = [[], [], [], [], [], []]
 
     dts = [[], [], [], [], [], []]
     vls = [[], [], [], [], [], []]
 
-    if len(missing_ch) is not 6:
+    if len(missing_ch) != 6:
         for i in range(6):
             dts[i] += datetimes[i]
             vls[i] += values[i]
@@ -112,7 +148,7 @@ def restruct_data(datetimes, values, missing_ch):
     return upload_struct
 
 
-def upload_protocol(sheet_no, date, type):
+def upload_protocol(sheet_no, date, data_type):
     """
 
     :param sheet_no: sheet object corresponding to ustruct data (T, P, R, etc.)
@@ -120,9 +156,9 @@ def upload_protocol(sheet_no, date, type):
     :param type: the data type to parse (i.e. T = temperature, etc.)
     :return: boolean
     """
-
-    type_datetimes, type_values, type_missing_ch = parse_file(date, type)
-    type_struct = restruct_data(type_datetimes, type_values, type_missing_ch)
+    client = gspread.authorize(creds)
+    type_datetimes, type_values, type_missing_ch = parse_file(date, data_type)
+    type_struct = restructure_data(type_datetimes, type_values, type_missing_ch)
     sheet = client.open('Fridge Data Testing').get_worksheet(sheet_no)
 
     # pressure_sheet = client.open('Fridge Data').get_worksheet(1)
@@ -130,7 +166,7 @@ def upload_protocol(sheet_no, date, type):
 
     print('uploading...')
     for key, val in type_struct.items():
-        time.sleep(2)
+        time.sleep(10)
         d = key.strftime('%m/%d/%y')
         t = key.strftime('%H:%M:%S')
         data = [d, t]
@@ -146,6 +182,7 @@ def upload_protocol(sheet_no, date, type):
 
 def main():
     # datetime.now().strftime('%y-%m-%d'), date = '17-02-28' for testing
+    # maxigague contains all info for pressue values
     date = datetime.now().strftime('%y-%m-%d')
     # type param should be of format ' X '
     upload_threads = []
@@ -165,9 +202,9 @@ def main():
 
 main()
 
+
 # comment out scheduling block for testing
 schedule.every(10).minutes.do(main)
-schedule.every(30).minutes.do(gspread.authorize(creds))
 while True:
     schedule.run_pending()
     time.sleep(1)
